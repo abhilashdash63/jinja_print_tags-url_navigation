6bc297b10c9f3c9a10ee9864c5e536f691421f8c
abhilashdash63 <abhilashdash63@gmail.com>
2023-07-31 12:06:29 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def data_render(request):
+    d={'name':'ABHILASH','age':23,'hobbies':['GAMING','football']}
+    return render(request,'data_render.html',context=d)
+
+
+def conditions(request):
+    d={'a':10000,'b':1500,'c':200}
+    return render(request,'conditions.html',context=d)
